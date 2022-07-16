9f5812ae0bb5d704a6848bc0402a6c59fd5309b2
trashmaggot <ilyaskazatov16@mail.ru>
2022-07-16 14:51:23 +0600

    Fixed CatTable

--- src/CatTable.java
+++ src/CatTable.java
@@ -8,16 +8,17 @@ public class CatTable {
     public void printTable(Comparator<Cat> comparator) {
         cats.sort(comparator);
         cats.forEach(cat -> {
-            if (cat.isHasPlayed()) System.out.println("You played with " + cat.getName() + " " + cat.getAge() + " years old");
+            if (cat.isHasPlayed())
+                System.out.println("You played with " + cat.getName() + " " + cat.getAge() + " years old");
             if (cat.isHasFeed()) System.out.println("You feed " + cat.getName() + " " + cat.getAge() + " years old");
-            if (cat.isHasVet()) System.out.println("You took to Vet " + cat.getName() + " " + cat.getAge() + " years old");
+            if (cat.isHasVet())
+                System.out.println("You took to Vet " + cat.getName() + " " + cat.getAge() + " years old");
         });
         System.out.println("---+----------+-----------+-----------+----------+----------+----------+");
         System.out.println(" # |   имя    |  возраст  | здоровье  |настроение|  сытость |ср уровень|");
         for (int i = 0; i < cats.size(); i++) {
             Cat cat = cats.get(i);
-            System.out.printf(" %2s| %8s |    %3s    |    %3s    |    %3s   |    %3s   |    %3s   |\n",
-                    i + 1, cat.getName(), cat.getAge(), cat.getHealthLevel(), cat.getMoodLevel(), cat.getSatietyLevel(), cat.getAverageLevel());
+            System.out.printf(" %2s| %8s |    %3s    |    %3s    |    %3s   |    %3s   |    %3s   |\n", i + 1, cat.getName(), cat.getAge(), cat.getHealthLevel(), cat.getMoodLevel(), cat.getSatietyLevel(), cat.getAverageLevel());
         }
         System.out.println("---+----------+-----------+-----------+----------+----------+----------+");
         System.out.println();
@@ -25,8 +26,7 @@ public class CatTable {
 
     public void addCat(String name, int age) {  //TODO
         Random r = new Random();
-        cats.add(new Cat(name, age, r.nextInt(81) + 20,
-                r.nextInt(81) + 20, r.nextInt(81) + 20));
+        cats.add(new Cat(name, age, r.nextInt(81) + 20, r.nextInt(81) + 20, r.nextInt(81) + 20));
     }
 
     public Cat getCat(int index) {
